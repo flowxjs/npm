e3d9a4931f48997d34726b2fd7b87a308759cc35
shenyunjie <shenyunjie@aidigger.com>
2020-05-18 14:00:29 +0800

    update

--- src/adapters/http/controller/user.controller.ts
+++ src/adapters/http/controller/user.controller.ts
@@ -4,15 +4,16 @@ import { Controller, Http, HttpCode, useException, Put, useMiddleware, Body, Get
 import { THttpContext } from '../../../app.bootstrap';
 import { logException } from '../exceptions/log.exception';
 import { TUserLoginInput, TUserLoginOutput, TUserInfoOutput } from './user.dto';
-import { ConfigController } from '../../../modules/configs/config.controller';
 import { UserController } from '../../../modules/user/user.controller';
 import { UserEntity } from '../../../modules/user/user.mysql.entity';
+import { ConfigService } from '../../../modules/configs/config.service';
 import { AccountPipe } from '../pipes/account';
 
 @Controller('/-/user')
 @useException(logException)
 export class HttpUserController {
   @inject('Http') private http: Http<THttpContext>;
+  @inject(ConfigService) private ConfigService: ConfigService;
 
   @HttpCode(201)
   @useMiddleware(BodyParser())
@@ -20,7 +21,7 @@ export class HttpUserController {
   async AddUser(@Body() body: TUserLoginInput): Promise<TUserLoginOutput> {
     if (!body.email) return;
     const rev = Buffer.from(body.name + ':' + body.password, 'utf8').toString('base64');
-    const configs: { loginType: string } = await this.http.portal(ConfigController, 'configs');
+    const configs = await this.ConfigService.query();
     if (configs.loginType !== 'default') throw new NotAcceptableException('目前不支持这种登录方式');
     await this.http.portal(UserController, 'passwordlogin', body.name, body.password, body.email, body.name, undefined, 'default');
     return {
